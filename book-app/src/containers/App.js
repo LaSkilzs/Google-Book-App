6784ca0a6b50889171b72a0347543fd933af10d9
La Fountain <github email address>
2019-06-18 12:18:30 -0400

    Reorganize file structure and added auxilary components

--- book-app/src/containers/App.js
+++ book-app/src/containers/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Dashboard from "./Dashboard";
-
-import "./App.css";
+import Book from "../presentational/Book";
+import "./css/App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>Google Api</h1>
       <Dashboard />
+      <Book />
     </div>
   );
 }
