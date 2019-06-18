8064335e5c965c2aa3f0d85b7de28e0b8641e1a8
La Fountain <github email address>
2019-06-18 12:04:40 -0400

    install Material ui and basic components structure and hierarchy set up

---
+++ book-app/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Dashboard from "./Dashboard";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Google Api</h1>
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
