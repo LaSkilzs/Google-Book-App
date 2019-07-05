f8dbcba0960c0adcd870e4f43f2998466b4f0c1f
La Fountain <github email address>
2019-07-05 13:29:09 -0400

    completed mobile readiness for ui design

--- bookapp/src/containers/App.js
+++ bookapp/src/containers/App.js
@@ -6,7 +6,7 @@ import Header from "../components/Header";
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
+      <div className="App" style={{ background: "fixed" }}>
         <Header />
         <Dashboard />
       </div>
--- bookapp/src/containers/Books.js
+++ bookapp/src/containers/Books.js
@@ -9,6 +9,24 @@ import {
   Link
 } from "@material-ui/core";
 
+import { createMuiTheme, MuiThemeProvider } from "@material-ui/core/styles";
+import createBreakpoints from "@material-ui/core/styles/createBreakpoints";
+
+const breakpoints = createBreakpoints({});
+const theme = createMuiTheme({
+  breakpoints,
+  overrides: {
+    MuiCard: {
+      root: {
+        margin: "auto",
+        [breakpoints.up("sm")]: {
+          margin: 0
+        }
+      }
+    }
+  }
+});
+
 const Books = props => {
   function FormRow() {
     return (
@@ -16,56 +34,58 @@ const Books = props => {
         {props.bookLists.map(book => {
           return (
             <Grid item xs={12} sm={6} md={4} key={book.infoLink}>
-              <Card>
-                {book.authors ? (
-                  <CardHeader
-                    title={book.title}
-                    subheader={book.authors.map(author => author)}
-                  />
-                ) : (
-                  <CardHeader title={book.title} />
-                )}
+              <MuiThemeProvider theme={theme}>
+                <Card>
+                  {book.authors ? (
+                    <CardHeader
+                      title={book.title}
+                      subheader={book.authors.map(author => author)}
+                    />
+                  ) : (
+                    <CardHeader title={book.title} />
+                  )}
 
-                {book.publisher ? (
+                  {book.publisher ? (
+                    <CardContent>
+                      <Typography
+                        variant="body2"
+                        color="textSecondary"
+                        component="p"
+                      >
+                        {book.publisher}
+                      </Typography>
+                    </CardContent>
+                  ) : null}
+
+                  {book.imageLinks ? (
+                    <CardMedia
+                      image={book.imageLinks.smallThumbnail}
+                      style={{ height: 170, width: 100, margin: "auto" }}
+                    />
+                  ) : (
+                    <CardMedia
+                      image="https://islandpress.org/sites/default/files/400px%20x%20600px-r01BookNotPictured.jpg"
+                      style={{ height: 170, width: 100, margin: "auto" }}
+                    />
+                  )}
+
                   <CardContent>
                     <Typography
                       variant="body2"
                       color="textSecondary"
                       component="p"
                     >
-                      {book.publisher}
+                      <Link
+                        href={book.infoLink}
+                        color="inherit"
+                        onClick={() => props.handleRefresh()}
+                      >
+                        {"see more info"}
+                      </Link>
                     </Typography>
                   </CardContent>
-                ) : null}
-
-                {book.imageLinks ? (
-                  <CardMedia
-                    image={book.imageLinks.smallThumbnail}
-                    style={{ height: 170, width: 100, margin: "auto" }}
-                  />
-                ) : (
-                  <CardMedia
-                    image="https://islandpress.org/sites/default/files/400px%20x%20600px-r01BookNotPictured.jpg"
-                    style={{ height: 170, width: 100, margin: "auto" }}
-                  />
-                )}
-
-                <CardContent>
-                  <Typography
-                    variant="body2"
-                    color="textSecondary"
-                    component="p"
-                  >
-                    <Link
-                      href={book.infoLink}
-                      color="inherit"
-                      onClick={() => props.handleRefresh()}
-                    >
-                      {"see more info"}
-                    </Link>
-                  </Typography>
-                </CardContent>
-              </Card>
+                </Card>
+              </MuiThemeProvider>
             </Grid>
           );
         })}
